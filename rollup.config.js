355fb78f54194cb6c0e1b2951971302977f58a9c
Hasibur Rahman <hasib.webdev@gmail.com>
2023-07-31 16:47:39 +0600

    init

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import sass from 'rollup-plugin-sass';
+import typescript from 'rollup-plugin-typescript2';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.tsx',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+      strict: false
+    },
+    {
+      file: pkg.module,
+      format: 'es'
+    }
+  ],
+  plugins: [
+    sass({ insert: true }),
+    typescript(),
+    commonjs(),
+    nodeResolve()
+  ],
+  external: ['react', 'react-dom']
+};
