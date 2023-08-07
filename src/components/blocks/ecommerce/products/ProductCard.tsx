942d334279e5673220f3074d469c92c431705433
Hasibur Rahman <hasib.webdev@gmail.com>
2023-08-07 13:49:47 +0600

    updates addons config

--- src/components/blocks/ecommerce/products/ProductCard.tsx
+++ src/components/blocks/ecommerce/products/ProductCard.tsx
@@ -37,7 +37,7 @@ const ProductCard: FC<ProductCardProps> = ({
                   ? generateImageUrl(product.media[0].path)
                   : '/images/placeholder.png'
               }
-              fill={true}
+              // fill={true}
               alt="product-image"
             />
           </div>
