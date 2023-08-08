f05294af0adc33a3b64dd56832835109ee6a0a6e
Hasibur Rahman <hasib.webdev@gmail.com>
2023-08-08 11:40:41 +0600

    filtered products

--- src/components/blocks/ecommerce/products/ProductGrid.tsx
+++ src/components/blocks/ecommerce/products/ProductGrid.tsx
@@ -3,7 +3,7 @@ import { Product } from '../../../../types/catalog';
 import ProductCard from './ProductCard';
 import { useBuilderConfigContext } from '@/contexts/config-context';
 
-const ProductGrid = () => {
+const ProductGrid = ({ data }) => {
   const [products, setProducts] = useState<Product[]>([]);
   const { axios, catalogConf } = useBuilderConfigContext();
   const fetchData = async () => {
@@ -20,9 +20,16 @@ const ProductGrid = () => {
 
   return (
     <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-8">
-      {products?.map((product, index) => {
-        return <ProductCard product={product} key={index} />;
-      })}
+      {products
+        ?.filter((c, index) => {
+          if (Boolean(data.show_custom_items) && data?.custom_items?.length) {
+            return data.custom_items.includes(c.id);
+          }
+          return index < Number(data.max_item);
+        })
+        .map((product, index) => {
+          return <ProductCard product={product} key={index} />;
+        })}
     </div>
   );
 };
